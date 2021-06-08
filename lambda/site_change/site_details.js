999a89521e1300d51254dfcb848082396a1d2f93
agilan2001 <agilanvlr2001@gmail.com>
2021-06-08 20:11:38 +0530

    update selectors

--- lambda/site_change/site_details.js
+++ lambda/site_change/site_details.js
@@ -3,7 +3,7 @@ exports.site_details = [
     {
         site_title:"ACOE",
         site_url:"https://acoe.annauniv.edu/",
-        compare_reg:["body > div:nth-child(3) > div > div > div","body > div:nth-child(4) > div > div.col-md-4"]
+        compare_reg:["body > div:nth-child(3)","body > div:nth-child(4) > div > div.col-md-4"]
     },
     {
         site_title:"CAC",
