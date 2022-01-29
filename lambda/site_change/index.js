8aac98c9f378cc23d8a60dd66d511f7b15111c15
agilan2001 <agilanvlr2001@gmail.com>
2022-01-29 14:07:54 +0530

    git based flow

--- lambda/site_change/index.js
+++ lambda/site_change/index.js
@@ -1,31 +1,10 @@
-var admin = require("firebase-admin");
 var nodemailer = require("nodemailer");
 var fetch = require("node-fetch");
 var {site_details} = require('./site_details');
 var {JSDOM} = require("jsdom");
 var sm = require("string-mismatch");
 var greedyCheck = new sm.Greedy();
-
-var serviceAccount = {
-    "type": "service_account",
-    "project_id": "site-change-check",
-    "private_key_id": "f3e1568d8f3968931eb478f0f279e101e6fe7c67",
-    //"private_key": "--STORED IN LAMBDA ENV VARIABLE--",
-    "client_email": "firebase-adminsdk-xgxit@site-change-check.iam.gserviceaccount.com",
-    "client_id": "115570318603166445899",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xgxit%40site-change-check.iam.gserviceaccount.com"
-}
-serviceAccount["private_key"] = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://site-change-check-default-rtdb.firebaseio.com"
-});
-
-var db = admin.database()
+var fs = require('fs');
 
 exports.handler = async (event) => {
 
@@ -36,7 +15,7 @@ exports.handler = async (event) => {
     var change_det = false;
 
     
-    // compare site_data from db and live fetched site_data and look for changes in the specified regions
+    // compare site_data from history and live fetched site_data and look for changes in the specified regions
 
     var diff = []
 
@@ -50,25 +29,26 @@ exports.handler = async (event) => {
             //if specified region is not found, compare and store the entire body
             var site_data = e.compare_reg.reduce((acc, cur) => (acc + (site_document.querySelector(cur)||site_document.querySelector('body')).textContent), "").trim()
 
-            db.ref("sites/" + e.site_title + "/data").once("value").then(snap => {
-                db_data = snap.val();
-                if (db_data == site_data) {
+            fs.readFile("./sites/" + e.site_title + ".json", (err,prev_data) => {
+                var prev_site_data = JSON.parse(prev_data.toString()).data;
+                if (prev_site_data == site_data) {
                     resolve(false)
                 } else {
                     change_det = true;
 
                     // compute and store the first 5 differences
                     cur_diff = [];
-                    greedyCheck.differences(db_data || "", site_data).slice(0,5).forEach((p,q)=>{
+                    greedyCheck.differences(prev_site_data || "", site_data).slice(0,5).forEach((p,q)=>{
                         if(p.type == "eql") return;
                         cur_diff.push(p)
                     })
                     diff[i] = cur_diff;
-                    db.ref("sites/" + e.site_title).update({
+
+                    fs.writeFile("./sites/" + e.site_title + ".json",JSON.stringify({
                         data: site_data, 
                         lst_upd: Date.now(), url: e.site_url,
                         last_diff:cur_diff
-                    }).then(() => resolve(true));
+                    },null,2), ()=>resolve(true))
                 }
             })
         })
@@ -111,15 +91,14 @@ exports.handler = async (event) => {
             </ul>
         `
         };
-        await transporter.sendMail(mailOptions);
+        // await transporter.sendMail(mailOptions);
     }
 
-    return ({
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-        },
-        statusCode: 200,
-        body: JSON.stringify(stat),
-    });
+    var proc = require('child_process')
+    proc.execSync("git config user.name agilan2001")
+    proc.execSync("git config user.email agilanvlr2001@gmail.com")
+    proc.execSync(`git commit -am "Update Diffs"`)
+
+    return stat;
 
 };
